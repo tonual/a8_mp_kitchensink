1754f28b830f0ed3aeed3c6e1d1f1d8df1fa7c6c
tonual <tonual@gmail.com>
2025-10-25 20:33:59 +0200

    relocator

--- mpt_samples_worklfow/pas/mptb.pas
+++ mpt_samples_worklfow/pas/mptb.pas
@@ -4,7 +4,7 @@ Const
   md1_player = $3000;
   module_addr = $5000;
   sample_addr = $6000;
-  
+
   module_filenames : array [0..2] Of string = ('TNL.MD1', 'BLUE3.MD1', 'TRANSIL.MD1');
   sample_filenames : array [0..2] Of string = ('TNL.D15', 'BLUE3.D15', 'TRANSIL.D15');
 
@@ -13,7 +13,72 @@ Var
   ch: char;
   song_index: byte = 0;
 
-{$r md1_play.rc}
+{$r mptb.rc}
+//md1 player resource
+
+Procedure RelocateMD1(modul: pointer; new_addr: word);
+Var 
+  old_add: word;
+  oalength: word;
+  ofs: word = 6;
+  tmp: word;
+  hlp: word;
+  i: byte;  
+  ph: pointer;
+  nadp: pointer;
+
+Begin  
+  If PWord(modul)^ <> $FFFF Then
+    Begin
+      writeln('Bad file format');
+      Halt;
+    End;
+
+  old_add := PWord(pointer(word(modul) + 2))^;
+  oalength := PWord(pointer(word(modul) + 4))^ - old_add + 1;  
+  // instruments  
+  For i := 0 To 31 Do
+    Begin
+      tmp := PWord(pointer(word(modul) + ofs + i*2))^;
+      If tmp <> 0 Then
+        Begin
+          hlp := tmp - old_add + new_addr;
+          PWord(pointer(word(modul) + ofs + i*2))^ := hlp;          
+        End;
+    End;  
+  // patterns  
+  For i := 0 To 63 Do
+    Begin
+      tmp := PWord(pointer(word(modul) + ofs + $40 + i*2))^;
+      If tmp <> 0 Then
+        Begin
+          hlp := tmp - old_add + new_addr;
+          PWord(pointer(word(modul) + ofs + $40 + i*2))^ := hlp;          
+        End;
+    End;  
+  // 4 tracks  
+  For i := 0 To 3 Do
+    Begin
+      tmp := PByte(pointer(word(modul) + ofs + $1C0 + i))^ + 
+        PByte(pointer(word(modul) + ofs + $1C4 + i))^ shl 8;
+      If tmp <> 0 Then
+        Begin
+          hlp := tmp - old_add + new_addr;
+          PByte(pointer(word(modul) + ofs + $1C0 + i))^ := Lo(hlp);
+          PByte(pointer(word(modul) + ofs + $1C4 + i))^ := Hi(hlp);          
+        End;
+    End;  
+  
+  // Shift the data down over the header to remove 
+  //the 6-byte Atari DOS header
+  ph := pointer(word(modul) + ofs);  
+  Move(ph^, modul^, oalength);  
+  // Move the relocated modul (without header) to the target address  
+  nadp := pointer(new_addr);
+  Move(modul^, nadp^, oalength);  
+
+End;
+
 
 //DOS II+/D Version 6.4 (c) '87 by S.D.
 Function LoadFileToAddr(Const filename: String; addr: word): Boolean;
@@ -37,14 +102,14 @@ Begin
 
   Repeat
     p := pointer(addr);
-    BlockRead(f, buf, SizeOf(buf), bytesRead);    
+    BlockRead(f, buf, SizeOf(buf), bytesRead);
     Move(buf, p^, bytesRead);
     addr := addr + bytesRead;
     totalRead := totalRead + bytesRead;
   Until bytesRead < SizeOf(buf);
   Close(f);
 {$I+}
-  
+
   writeln('total bytes read: ',totalRead);
   Result := true;
 End;
@@ -60,15 +125,13 @@ Begin
 
   SetIntVec(iVBL, @vbl);
 
-  While song_index < 4 Do
+  While song_index < 3 Do
     Begin
-      writeln('Loading song ', module_filenames[song_index]);      
+      writeln('Loading song ', module_filenames[song_index]);
       LoadFileToAddr(module_filenames[song_index], module_addr);
+      RelocateMD1(pointer(module_addr), module_addr);    
       LoadFileToAddr(sample_filenames[song_index], sample_addr);
-
-      writeln('Loaded..press any key');
-      ch := readkey;
-
+      
       msx.player := pointer(md1_player);
       msx.modul := pointer(module_addr);
       msx.sample := pointer(sample_addr);
