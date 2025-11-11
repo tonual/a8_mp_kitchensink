c956085e4a87f4e8bad3b90f06cb6bcffd55616c
tonual <tonual@gmail.com>
2025-11-11 18:52:04 +0100

    digimsx volumes

--- mpt_samples_worklfow/pas/mptb.sh
+++ mpt_samples_worklfow/pas/mptb.sh
@@ -4,9 +4,9 @@ echo "> Compiling browser"
 mpb mptb.pas
 echo "> Creating .atr"
 
-./mkatr digimsx.atr -b +ph bwdos/xbw130.dos +p bwdos/startup.bat -s 524288 output/mptb.xex ../song/1-channel-digi/VOL1/*
-#./mkatr digimsx.atr -b +ph bwdos/xbw130.dos +p bwdos/startup.bat -s 524288 output/mptb.xex ../song/1-channel-digi/VOL2/*
-#./mkatr digimsx.atr -b +ph bwdos/xbw130.dos +p bwdos/startup.bat -s 524288 output/mptb.xex ../song/1-channel-digi/VOL3/*
+#./mkatr dimsxvo1.atr -b +ph bwdos/xbw130.dos +p bwdos/startup.bat -s 524288 output/mptb.xex ../song/1-channel-digi/VOL1/*
+./mkatr dimsxvo2.atr -b +ph bwdos/xbw130.dos +p bwdos/startup.bat -s 524288 output/mptb.xex ../song/1-channel-digi/VOL2/*
+#./mkatr dimsxvo3.atr -b +ph bwdos/xbw130.dos +p bwdos/startup.bat -s 524288 output/mptb.xex ../song/1-channel-digi/VOL3/*
 
 
 #
