0099eeb1cb26cb4dd7d87e31f2ba5b20158549d5
tonual <tonual@gmail.com>
2025-11-11 15:47:18 +0100

    fix atr size

--- mpt_samples_worklfow/pas/mptb.sh
+++ mpt_samples_worklfow/pas/mptb.sh
@@ -4,8 +4,11 @@ echo "> Compiling browser"
 mpb mptb.pas
 echo "> Creating .atr"
 
-./mkatr digimsx.atr -b +ph bwdos/xbw130.dos +p bwdos/startup.bat output/mptb.xex ../song/1-channel-digi/VOL1/*
-#./mkatr digimsx.atr -b +ph bwdos/xbw130.dos +p bwdos/startup.bat output/mptb.xex ../song/1-channel-digi/VOL2/*
-#./mkatr digimsx.atr -b +ph bwdos/xbw130.dos +p bwdos/startup.bat output/mptb.xex ../song/1-channel-digi/VOL3/*
+./mkatr digimsx.atr -b +ph bwdos/xbw130.dos +p bwdos/startup.bat -s 524288 output/mptb.xex ../song/1-channel-digi/VOL1/*
+#./mkatr digimsx.atr -b +ph bwdos/xbw130.dos +p bwdos/startup.bat -s 524288 output/mptb.xex ../song/1-channel-digi/VOL2/*
+#./mkatr digimsx.atr -b +ph bwdos/xbw130.dos +p bwdos/startup.bat -s 524288 output/mptb.xex ../song/1-channel-digi/VOL3/*
+
+
+#
 
 echo "> Done!"
